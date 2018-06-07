8ca54d3e26cee3e3459da510c397490d985909cb
Zach Moroski <37152890+ZachMoroski@users.noreply.github.com>
2018-06-07 13:43:39 -0400

    Still Working on Block Filler Fixing

--- src/main/java/zachsmods/commands/BlockFillerPositionSelector.java
+++ src/main/java/zachsmods/commands/BlockFillerPositionSelector.java
@@ -2,20 +2,19 @@ package zachsmods.commands;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import net.minecraft.entity.ai.EntityMoveHelper.Action;
 import net.minecraft.init.Items;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLMissingMappingsEvent.Action;
 
 public class BlockFillerPositionSelector {
 	
 	static List<Integer> pos1 = new ArrayList();
 	static List<Integer> pos2 = new ArrayList();
 
-	@SubscribeEvent
+	@EventHandler
 	public void choosePositions(PlayerInteractEvent event) {
 		if (event.getEntityPlayer().getHeldItemMainhand() == null
 				|| event.getEntityPlayer().getHeldItemMainhand().getItem() != Items.WOODEN_AXE
@@ -23,8 +22,7 @@ public class BlockFillerPositionSelector {
 			return;
 		}
 		
-		Action action = event.action;
-		if (action == Action.LEFT_CLICK_BLOCK) {
+		if(event.getResult() == Action.LEFT_CLICK_BLOCK) {
 			pos1.clear();
 			pos1.add(event.getPos().getX());
 			pos1.add(event.getPos().getY());
@@ -33,7 +31,7 @@ public class BlockFillerPositionSelector {
 							+ ", " + event.getPos().getY() + ", " + event.getPos().getZ() + "."));
 			event.setCanceled(true);
 			
-		} else if (action == Action.RIGHT_CLICK_BLOCK) {
+		} else if (event.action == Action.RIGHT_CLICK_BLOCK) {
 			pos2.clear();
 			pos2.add(event.getPos().getX());
 			pos2.add(event.getPos().getY());
