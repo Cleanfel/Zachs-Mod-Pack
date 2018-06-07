60bf2048690f9ee647f4e0f955564e095a0cad0f
Zach Moroski <37152890+ZachMoroski@users.noreply.github.com>
2018-06-07 14:56:45 -0400

    Added Accepted Versions to Main

--- src/main/java/zachsmods/main/Main.java
+++ src/main/java/zachsmods/main/Main.java
@@ -35,12 +35,13 @@ import zachsmods.tabs.TabOfTools;
 import zachsmods.tutorial.BlockBreakMessage;
 import zachsmods.tutorial.DiamondMiner;
 
-@Mod(modid = Main.MOD_ID, name = Main.MOD_NAME, version = Main.VERSION)
+@Mod(modid = Main.MOD_ID, name = Main.MOD_NAME, version = Main.VERSION, acceptedMinecraftVersions = Main.ACCEPTED_VERSIONS)
 public class Main {
 
 	public static final String MOD_ID = "zachsmods";
 	public static final String MOD_NAME = "Zach's Mod Pack";
 	public static final String VERSION = "1.0.0";
+	public static final String ACCEPTED_VERSIONS = "[1.9.4]";
 	
 	@Instance
 	public static Main instance = new Main();
